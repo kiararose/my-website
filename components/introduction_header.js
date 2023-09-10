cb7c7d05be2561afd1faf28fcf379352769b8e2c
kiararose <kiararxsemarcelo@gmail.com>
2023-09-11 06:36:55 +0800

    feat: monkey peeking image, updated max width of container

--- components/introduction_header.js
+++ components/introduction_header.js
@@ -3,6 +3,9 @@ export default function IntroductionHeader(props) {
 
     return (
         <div className="intro-header">
+			<div className="intro-header__image">
+				<img className="img-fluid" src="/images/monkey-peeking.png" alt="" />
+			</div>
 			<div className="intro-header__description">
 				<p>
 					{description}
